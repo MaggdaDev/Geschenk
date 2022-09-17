421c22a8c75f2bf3da0548f8ba6bd64bdc0e1b55
MaggdaDev <davidbetko05@gmail.com>
2022-09-17 15:04:18 +0200

    hh

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const http = require('http');
 
-const host = "minortom.net";
+const host = "minortom";
 const port = "12345";
 
+function redirect(link) {
+    return '<meta http-equiv="refresh" content="0; url=' + link + '" />';
+}
+
+function incrementCounter() {
+    counter += 1;
+    counter %= htmls.length;
+}
+
+const htmls = [
+    redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"),
+    redirect("https://www.youtube.com/watch?v=djV11Xbc914&ab_channel=a-ha"),
+    redirect("https://www.youtube.com/watch?v=k3BWCHB2QZg&ab_channel=Galileel")
+
+]
+var counter = 0;
+var lastGet = Date.now();
 const requestListener = function (req, res) {
-    res.writeHead(200);
-    res.end("My first server!");
+    if (req.rawHeaders[10].includes('Insecure')) {
+        res.writeHeader(200, { "Content-Type": "text/html" });
+        res.write(htmls[counter]);
+        console.log("Sending " + counter + "th");
+        incrementCounter();
+        res.end();
+    }
 };
 
 const server = http.createServer(requestListener);
